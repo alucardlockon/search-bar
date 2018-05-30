18102cc1c31f5a8f91cde6b1426f95e7813daa12
yuwh <alucardlockon3@gmail.com>
2018-05-31 02:04:46 +0800

    init

---
+++ serach-abr/serach-abr/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  serach-abr
+//
+//  Created by 于文辉 on 2018/5/31.
+//  Copyright © 2018年 alucardlockon. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ serach-abr/serach-abr/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  serach-abr
+//
+//  Created by 于文辉 on 2018/5/31.
+//  Copyright © 2018年 alucardlockon. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
